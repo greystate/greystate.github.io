640261026a55f10e7191f0097e4d658ae66dd024
Chriztian Steinmeier <chriztian@steinmeier.dk>
2017-10-10 21:24:41 +0200

    Tweak the XSLT syntax definitions

--- src/vendor/prism/prism-xslt.js
+++ src/vendor/prism/prism-xslt.js
@@ -1,7 +1,9 @@
 Prism.languages.xslt = Prism.languages.extend('markup', {
-	'constant': /\$\w+/i,
-	'attr-selector': /@\w+/i
+	'param': /\$\w+/i,
+	'attr-selector': /@\w+/i,
+	'function': /\b(string|not|boolean|generate-id|translate)/
 });
 
-Prism.languages.xslt['tag'].inside['attr-value'].inside['constant'] = Prism.languages.xslt['constant'];
-Prism.languages.xslt['tag'].inside['attr-value'].inside['selector'] = Prism.languages.xslt['attr-selector'];
+Prism.languages.xslt['tag'].inside['attr-value'].inside['constant'] = Prism.languages.xslt['param'];
+Prism.languages.xslt['tag'].inside['attr-value'].inside['operator'] = Prism.languages.xslt['attr-selector'];
+Prism.languages.xslt['tag'].inside['attr-value'].inside['function'] = Prism.languages.xslt['function'];
